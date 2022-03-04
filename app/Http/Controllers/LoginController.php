f2c0446534d4e652146e7f48ce0399d3b4dcbe87
Jakub Volák <jakub.volak@seznam.cz>
2022-03-04 09:59:43 +0100

    Basic login action

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle an authentication attempt.
+     *
+     * @param Request $request
+     * @return Response|JsonResponse
+     */
+    public function authenticate(Request $request): Response|JsonResponse
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return response()->json(Auth::user());
+        }
+
+        return response('Bad credentials', 401);
+    }
+}
