22de44f3cdbb88036d6aee42bccd759ecab17e92
Jakub Volák <jakub.volak@seznam.cz>
2022-04-05 22:03:26 +0200

    Added exercise file download action

--- app/Http/Controllers/ExerciseFileController.php
+++ app/Http/Controllers/ExerciseFileController.php
@@ -6,9 +6,31 @@ use App\Models\ExerciseFile;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Storage;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class ExerciseFileController extends Controller
 {
+    /**
+     * Returns file by ID
+     *
+     * @param $id
+     * @return Response|StreamedResponse
+     */
+    public function download($id): Response|StreamedResponse
+    {
+        $exerciseFile = ExerciseFile::getFileByID($id);
+        if($exerciseFile == null) {
+            return response(['message' => trans('messages.fileDoesntExistError')], 404);
+        }
+
+        $path = 'exercises/' . $exerciseFile->exercise_id . '/' . $exerciseFile->file_name;
+        if(Storage::exists($path)) {
+            return Storage::download($path);
+        }
+
+        return response(['message' => trans('messages.fileDoesntExistError')], 404);
+    }
+
     /**
      * Returns response after success delete
      *
--- routes/api.php
+++ routes/api.php
@@ -104,6 +104,8 @@ Route::middleware(['localization', 'auth:sanctum', 'ability:admin'])->group(func
     Route::post('/exercise/{id}/upload', [ExerciseController::class, 'upload'])->name('upload_files_to_exercise');
 
 
+    Route::get('/exercise-file/{id}', [ExerciseFileController::class, 'download'])->name('download_exercise_file');
+
     Route::delete('/exercise-file/{id}', [ExerciseFileController::class, 'delete'])->name('delete_exercise_file');
 });
 
