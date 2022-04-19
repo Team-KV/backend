6ec3677befc86e096642f802a1e13103e6212310
Jakub Volák <jakub.volak@seznam.cz>
2022-04-19 17:40:21 +0200

    [ADD] Added default Controller.php with basic actions

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,9 +5,54 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * Returns JSON response with data
+     *
+     * @param $data
+     * @return JsonResponse
+     */
+    public function sendData($data): JsonResponse
+    {
+        return response()->json($data);
+    }
+
+    /**
+     * Returns response with no content
+     *
+     * @return Response
+     */
+    public function sendNoContent(): Response
+    {
+        return response('', 204);
+    }
+
+    /**
+     * Returns response with not found message
+     *
+     * @param string $message
+     * @return Response
+     */
+    public function sendNotFound(string $message): Response
+    {
+        return response(['message' => trans($message)], 404);
+    }
+
+    /**
+     * Returns response with conflict message
+     *
+     * @param string $message
+     * @return Response
+     */
+    public function sendConflict(string $message): Response
+    {
+        return response(['message' => trans($message)], 409);
+    }
 }
